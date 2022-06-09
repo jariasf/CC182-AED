b5c6bf8b294e8a038d2ce004bcd49c2389943417
jariasf <jariasf03@gmail.com>
2022-06-08 22:48:37 -0500

    Add binary tree and BST implementations

--- Semana 11/binaryTree.cpp
+++ Semana 11/binaryTree.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <vector>
 using namespace std;
 
 class TreeNode{
@@ -17,7 +18,7 @@ public:
     BinaryTree();
     ~BinaryTree();
     void createTreeTest();
-    void createTreeFromArray(int *A, int n);
+    void createTreeFromArray(vector<string> &A);
     int height();
     void inorder();
     void preorder();
@@ -27,7 +28,7 @@ public:
 
 private:
     TreeNode* root;
-    TreeNode* _createTreeFromArray(TreeNode* node, int *A, int pos, int n);
+    TreeNode* _createTreeFromArray(TreeNode* node, int pos, vector<string> &A);
     int _height(TreeNode* node); 
     void _preorder(TreeNode* node);
     void _inorder(TreeNode* node);
@@ -49,16 +50,28 @@ void BinaryTree::createTreeTest(){
     root -> left -> right = new TreeNode(65);
 }
 
+void BinaryTree::createTreeFromArray(vector<string> &A){
+    root = _createTreeFromArray(root, 0, A);    
+}
+
+TreeNode* BinaryTree::_createTreeFromArray(TreeNode* node, int pos, vector<string> &A){
+    if(pos >= A.size() || A[pos] == "null") return nullptr;
+    node = new TreeNode(stoi(A[pos]));
+    node -> left = _createTreeFromArray(node -> left, 2 * pos + 1, A);
+    node -> right = _createTreeFromArray(node -> right, 2 * pos + 2, A);  
+    return node;
+}
+
 BinaryTree::~BinaryTree(){
     destroyTree(root);
 }
 
-void BinaryTree::destroyTree(TreeNode *root){
-    if( root == nullptr) return;
-    destroyTree(root -> left);
-    destroyTree(root -> right);
-    delete root;
-    root = nullptr;
+void BinaryTree::destroyTree(TreeNode *node){
+    if( node == nullptr) return;
+    destroyTree(node -> left);
+    destroyTree(node -> right);
+    delete node;
+    node = nullptr;
 }
 
 int BinaryTree::height(){
@@ -84,10 +97,15 @@ void BinaryTree::_inorder(TreeNode* node){
 
 void BinaryTree::preorder(){
     _preorder(root);
+    cout<<endl;
 }
 
 void BinaryTree::_preorder(TreeNode* node){
-    
+    if( node == nullptr )
+        return;
+    cout<<node -> val <<" "; 
+    _preorder(node -> left);
+    _preorder(node -> right);
 }
 
 void BinaryTree::postorder(){
@@ -102,19 +120,35 @@ int BinaryTree::countNodes(){
     return _countNodes(root);
 }
 
-int BinaryTree::_countNodes(TreeNode* root){
-    
+int BinaryTree::_countNodes(TreeNode* node){
+    if( node == nullptr )
+        return 0;
+    int nodos_left = _countNodes(node -> left);
+    int nodos_right = _countNodes(node -> right);
+    return 1 + nodos_left + nodos_right;
 }
 
 int BinaryTree::sumOfValues(){
     return _sumOfValues(root);
 }
 
-int BinaryTree::_sumOfValues(TreeNode* root){
-    
+int BinaryTree::_sumOfValues(TreeNode* node){
+    if( node == nullptr )
+        return 0;
+    int sum_left = _sumOfValues(node -> left);
+    int sum_right = _sumOfValues(node -> right);
+    return node -> val + sum_left + sum_right;
 }
 
 int main(){
-    
+    BinaryTree* tree = new BinaryTree();
+    vector<string> A = {"5", "1", "-4", "12", "null", "34", "null"};
+    tree -> createTreeFromArray(A);
+    cout<<"Preorder traversal"<<endl;
+    tree -> preorder();
+
+    cout<<"\nNumber of nodes: "<<tree -> countNodes()<<endl;
+    cout<<"Sum of values: "<<tree -> sumOfValues()<<endl;
+    delete tree;
     return 0;
 }
--- Semana 12/BST.cpp
+++ Semana 12/BST.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+﻿#include <iostream>
+#include <queue>
 using namespace std;
 
 class TreeNode{
@@ -15,15 +16,25 @@ public:
 class BST{
     public:
     BST();
+    //BST(function<int(T,T)> compare);
     ~BST();
     bool search(int key);
     bool insert(int key);
- 
+    void inorder();
+    void preorder();
+    void postorder();
+    bool deleteNode(int key);
+    TreeNode* inorderSuccessor(TreeNode* node);
+    void levelOrderTraversal();
+
     private:
     TreeNode* root;
-    bool _search(TreeNode* node, int key);
-    bool _insert(TreeNode* &node, int key);
-    TreeNode* _insert2(TreeNode* node, int key);
+    //function<int(T,T)> compare;
+    bool search(TreeNode* node, int key);
+    bool insert(TreeNode* &node, int key);
+    void inorder(TreeNode* node);
+    bool deleteNode(TreeNode* &node, int key);
+    TreeNode* successor(TreeNode* node);
     void destroyTree(TreeNode* node);
 };
 
@@ -44,28 +55,127 @@ void BST::destroyTree(TreeNode *root){
 }
 
 bool BST::search(int key){
-    return _search(root, key);
+    return search(root, key);
 }
 
-bool BST::_search(TreeNode* node, int key){
+bool BST::search(TreeNode* node, int key){
     if( node == nullptr ){
         return false;
     }else if( node -> key == key ){
         return true;
     }else if( node -> key > key ){
-        return _search(node -> left, key);
+        return search(node -> left, key);
     }
-    return _search(node -> right, key);
+    return search(node -> right, key);
 }
 
 bool BST::insert(int key){
-    return _insert(root, key);
+    return insert(root, key);
 }
 
-bool BST::_insert(TreeNode* &node, int key){
+bool BST::insert(TreeNode* &node, int key){
+    if( node == nullptr ){
+        node = new TreeNode(key);
+        return true;
+    }
+    if( key == root -> key ){
+        return false;
+    }else if( key < node -> key ){
+        return insert(node -> left, key);
+    }
+    return insert(node -> right, key);
+}
 
+void BST::inorder(){
+    inorder(root);
+    cout<<endl;
+}
+
+void BST::inorder(TreeNode* node){
+    if( node == nullptr )
+        return ;
+    inorder(node -> left);
+    //process(node -> key);
+    cout<<node -> key<<" ";
+    inorder(node -> right);
+}
+
+bool BST::deleteNode(int key){
+    return deleteNode(root, key);
+}
+
+TreeNode* BST::successor(TreeNode* node){
+    TreeNode* current = node -> right;
+    while( current && current -> left ){
+        current = current -> left;
+    }
+    return current;
+}
+
+bool BST::deleteNode(TreeNode* &node, int key){
+    if( node ==nullptr ){
+        return false;
+    }else if( node -> key > key){
+        return deleteNode(node -> left, key);
+    }else if( node -> key < key ){
+        return deleteNode(node -> right, key);
+    }else{
+        // Case 1: Leaf
+        if( node -> left == nullptr && node -> right == nullptr){
+            delete node;
+            node = nullptr;
+            return true;
+        }else if( node -> left == nullptr ){
+            // Case 2: Only right child
+            TreeNode* tmp = node;
+            node = node -> right;
+            delete tmp;
+            return true;
+        }else if( node -> right == nullptr){
+            // Case 2: Only left child
+            TreeNode* tmp = node;
+            node = node -> left;
+            delete tmp;
+            return true;
+        }
+        // Case 3: Two children
+        TreeNode* next = successor(node);
+        node -> key = next -> key;
+        return deleteNode(root -> right, next -> key);
+    }
+}
+
+void BST::levelOrderTraversal(){
+    queue<TreeNode*> Q;
+    Q.push(root);
+    while(!Q.empty()){
+        TreeNode* current = Q.front(); Q.pop();
+        cout<<current -> key<<" ";
+        if( current -> left ){
+            Q.push(current -> left);
+        }
+        if( current -> right){
+            Q.push(current -> right);
+        }
+    }
+    cout<<endl;
 }
 
 int main(){
+    BST *tree = new BST();
+    int n = 6;
+    int a[6] = {5,10,1,14,-1,6};
+    for(int i = 0 ; i < 6 ; ++i ){
+        tree -> insert(a[i]);
+    }
+    cout<<"Level order traversal"<<endl;
+    tree -> levelOrderTraversal();
+    cout<<"\nInorder traversal"<<endl;
+    tree -> inorder();
+    tree -> deleteNode(5);
+    tree -> deleteNode(-1);
+    tree -> deleteNode(10);
+    tree -> inorder();    
+    delete tree;
     return 0;
 }
